f5692f845fee67e5ac7057a68ad731a75711d2bb
pj <>
2019-12-17 11:21:24 +0545

    add authors macro

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,21 @@
-use clap::{crate_description, crate_name, crate_version, App, AppSettings/* , Arg */};
+use clap::{
+    crate_authors, /* , Arg */
+    crate_description, crate_name, crate_version, App, AppSettings,
+};
 
 pub fn new<'a, 'b>() -> App<'a, 'b> {
     App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
+        .author(crate_authors!())
         .setting(AppSettings::ArgRequiredElseHelp)
         .setting(AppSettings::ColorAuto)
-        // .arg(
-        //     Arg::with_name("option")
-        //         .short("o")
-        //         .long("option")
-        //         .help("option description")
-        //         .takes_value(true)
-        //         .value_name("opts"),
-        // )
+    // .arg(
+    //     Arg::with_name("option")
+    //         .short("o")
+    //         .long("option")
+    //         .help("option description")
+    //         .takes_value(true)
+    //         .value_name("opts"),
+    // )
 }
